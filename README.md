70062a2e1f6c658106dd4bfcf8f2b2e5f0f634c5
Ramon Perez <rapega12@gmail.com>
2019-10-11 10:28:39 +0200

    Added progress bar to montecarlo

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Poker simulator
 A small python project with a simple GUI to simulate poker games.
+poker.py is the base library and should only be executed for debugging.
 # Prerequisites
 PySimpleGUI
+tqdm
--- montecarlo.py
+++ montecarlo.py
@@ -2,22 +2,30 @@
 the probability of the different hands of poker.
 """
 import poker
-n = 10000 #Number of simulations
+from tqdm import tqdm
 hands = ['highest card', 'pair', 'two pair', 'three of a kind', 'straight',
          'flush', 'full house', 'four of a kind', 'straight flush']
-results = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-i = 0
-while i < n:
-    deck = poker.Deck()
-    deck.shuffle()
-    player = poker.Hand('Player')
-    table = poker.Hand('Table')
-    deck.move_cards(player, 2)
-    deck.move_cards(table, 5)
-    results[player.best_hand(table)[0]] += 1
-    i += 1
-for i in range(9):
-    if i == 1 or i == 5:
-        print(hands[i], '{0:.7f}'.format(results[i]/n), sep='\t\t')
-    else:
-        print(hands[i], '{0:.7f}'.format(results[i]/n), sep='\t')
+
+n = input('Number of simulations?\t')
+try:
+    n=int(n)
+except:
+    print('Value not valid')
+    exit(1)
+
+with open('montecarlo-results.txt', 'w') as fout:
+    results = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    print('Running simulations')
+    for i in tqdm(range(n)):
+        deck = poker.Deck()
+        deck.shuffle()
+        player = poker.Hand('Player')
+        table = poker.Hand('Table')
+        deck.move_cards(player, 2)
+        deck.move_cards(table, 5)
+        results[player.best_hand(table)[0]] += 1
+    for i in range(9):
+        if i == 1 or i == 5:
+            fout.write(hands[i] + '\t\t' + '{0:.7f}'.format(results[i]/n) + '\n')
+        else:
+            fout.write(hands[i] + '\t' + '{0:.7f}'.format(results[i]/n) + '\n')
